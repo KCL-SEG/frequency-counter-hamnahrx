653c7e7644f7710b9ee2fd4206e31b99ffacb007
“HAMNAH <hamnah.1.rassen@kcl.ac.uk>
2023-11-11 18:36:24 +0000

    Update frequencies.py
    Created frequencies functionality

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from collections import Counter
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    #loop through each item object in items list
+    for item in items:
+        #if item is not a string, convert item into string 
+        key = str(item) if not isinstance(item, str) else item
+
+        frequencies[key] = frequencies.get(key, 0) + 1
     return frequencies
